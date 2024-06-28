2377e04c4ee77d319f61aa72c056d8c464fe5eb7
Beatriz Herranz <167246883+BeatrizHerranz@users.noreply.github.com>
2024-06-28 18:13:28 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,44 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  // variables
+
+  const who = ["El perro", "Mi abuela", "El cartero", "Mi pájaro"];
+  const action = ["comió", "orinó", "aplastó", "rompió"];
+  const what = ["mi tarea", "mi teléfono", "el coche"];
+  const when = [
+    "antes de la clase",
+    "cuando estaba durmiendo",
+    "mientras hacía ejercicio",
+    "durante mi almuerzo",
+    "mientras estaba rezando"
+  ];
+
+  // Funciones
+
+  const getRandomElement = arr => {
+    return arr[Math.floor(Math.random() * arr.length)];
+  };
+
+  const generateExcuse = () => {
+    let randomWho = getRandomElement(who);
+    let randomAction = getRandomElement(action);
+    let randomWhat = getRandomElement(what);
+    let randomWhen = getRandomElement(when);
+
+    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+  };
+
+  // function generateExcuse() {
+  //   let randomWho = who[Math.floor(Math.random() * who.length)];
+  //   let randomAction = action[Math.floor(Math.random() * action.length)];
+  //   let randomWhat = what[Math.floor(Math.random() * what.length)];
+  //   let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  //   return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+  // }
+
+  //No tocar esta seccion del codigo
+  document.querySelector("#excuse").innerHTML = generateExcuse();
 };
